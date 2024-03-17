f7b278e14a445ac5ec41108e9909c454ed838bab
dikikanety <dikibudikanety@gmail.com>
2024-03-17 22:53:50 +0800

    setup deploy to github

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: '/FE-Test-Intern/',
   resolve: {
     alias: {
       // eslint-disable-next-line no-undef

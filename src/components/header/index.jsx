b4c2adff51f84eff4dca4ee1727f2bbb1b49a746
dikikanety <dikibudikanety@gmail.com>
2024-03-17 23:40:38 +0800

    fixing padding header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -14,7 +14,7 @@ export default function Header() {
       <Navigation />
       <div className="relative isolate z-10">
         <div className="overflow-hidden py-24 sm:py-32">
-          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-7xl px-6 lg:px-20">
             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
               <div className="pt-14 flex flex-col justify-center lg:max-w-lg">
                 <h1 className="self-stretch font-ubuntu-mono text-5xl font-normal tracking-tight text-black">
